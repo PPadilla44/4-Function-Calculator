39e94ddc7991201ede0c56f2b0359ac3c083e90f
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-04-26 15:58:39 -0700

    added button flare

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ var total = 0;
 
 var display = document.getElementById("display");
 
-function press(input) {
+
+function press(element,input) {
+
+    element.style.backgroundColor = "rgb(130, 130, 130)";
+    setTimeout( function changeColor(){ 
+    element.style.backgroundColor = "rgb(97, 97, 97)";
+
+    }, 125);
     if(!pressOp) {
         input1 += input
         display.innerHTML = input1;
@@ -20,12 +27,22 @@ function press(input) {
 }
 
 
-function setOP(operator) {
+function setOP(element, operator) {
+    element.style.backgroundColor = "rgb(255, 180, 30)";
+    setTimeout( function changeColor(){ 
+    element.style.backgroundColor = "rgb(255, 152, 17)";
+    }, 125);
+
     op = operator
     pressOp = true
 }
 
-function calculate() {
+function calculate(element) {
+    element.style.backgroundColor = "rgb(237, 237, 237)";
+    setTimeout( function changeColor(){ 
+    element.style.backgroundColor = "rgb(207, 207, 207)";
+    }, 125);
+
     var input1Num =  parseFloat(input1);
     var input2Num = parseFloat(input2);
 
@@ -46,14 +63,18 @@ function calculate() {
     if(total.length > 9) {
         display.innerHTML = total.substring(0,9) + "...";
     } else {
-        display.innerHTML = total
+        display.innerHTML = total;
     }
     input1 = total;
     input2 = "";
     
 }
 
-function clr() {
+function clr(element) {
+    element.style.backgroundColor = "rgb(130, 130, 130)"
+    setTimeout( function changeColor(){ 
+    element.style.backgroundColor = "rgb(97, 97, 97)"
+    }, 125);
     display.innerHTML = "0";
     input1 = "";
     input2 = "";
